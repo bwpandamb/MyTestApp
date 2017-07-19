43b57bf23ff28c0c7c7585e8e697fa2f821971ba
Charles Ma (External Consultant) <ext.charles.ma@EF.com>
2017-07-19 17:44:07 +0800

    add a function

--- MyApplication/app/src/main/java/com/example/achar/mytestapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/achar/mytestapp/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.achar.mytestapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+        iv = (ImageView) findViewById(R.id.iv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                /* 开启Pictures画面Type设定为image */
+                intent.setType("image/*");
+                /* 使用Intent.ACTION_GET_CONTENT这个Action */
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                /* 取得相片后返回本画面 */
+                startActivityForResult(intent, 1);
+
+            }
+        });
+
+
     }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode == 1) {
+                Uri uri = data.getData();
+                ContentResolver cr = this.getContentResolver();
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                    if (bitmap != null) {
+                        iv.setImageBitmap(bitmap);
+                    }
+                    /* 将Bitmap设定到ImageView */
+
+                } catch (FileNotFoundException e) {
+                    Log.e("Exception", e.getMessage(),e);
+                }
+            }
+            super.onActivityResult(requestCode, resultCode, data);
+        }
 }
